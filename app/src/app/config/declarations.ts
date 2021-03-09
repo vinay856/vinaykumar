a78c672ba800d457de3d3f4353165be7dde8b988
Tanguturu Vinay Kumar <vinay.kumar@ahana.co.in>
2021-03-08 20:30:38 -0800

    FirstDay

---
+++ app/src/app/config/declarations.ts
@@ -0,0 +1,76 @@
+import { NeutrinosAuthGuardService } from 'neutrinos-oauth-client';
+import { PageNotFoundComponent } from '../not-found.component';
+import { LayoutComponent } from '../layout/layout.component';
+import { ImgSrcDirective } from '../directives/imgSrc.directive';
+import { APP_INITIALIZER } from '@angular/core';
+import { NDataSourceService } from '../n-services/n-dataSorce.service';
+import { environment } from '../../environments/environment';
+import { NLocaleResource } from '../n-services/n-localeResources.service';
+import { NAuthGuardService } from 'neutrinos-seed-services';
+import { ArtImgSrcDirective } from '../directives/artImgSrc.directive';
+
+
+window['neutrinos'] = {
+  environments: environment
+}
+
+//CORE_REFERENCE_IMPORTS
+//CORE_REFERENCE_IMPORT-sessionpageComponent
+import { sessionpageComponent } from '../components/sessionpageComponent/sessionpage.component';
+
+/**
+ * Reads datasource object and injects the datasource object into window object
+ * Injects the imported environment object into the window object
+ *
+ */
+export function startupServiceFactory(startupService: NDataSourceService) {
+  return () => startupService.getDataSource();
+}
+
+/**
+*bootstrap for @NgModule
+*/
+export const appBootstrap: any = [
+  LayoutComponent,
+];
+
+
+/**
+*declarations for @NgModule
+*/
+export const appDeclarations = [
+  ImgSrcDirective,
+  LayoutComponent,
+  PageNotFoundComponent,
+  ArtImgSrcDirective,
+  //CORE_REFERENCE_PUSH_TO_DEC_ARRAY
+//CORE_REFERENCE_PUSH_TO_DEC_ARRAY-sessionpageComponent
+sessionpageComponent,
+
+];
+
+/**
+* provider for @NgModuke
+*/
+export const appProviders = [
+  NDataSourceService,
+  NLocaleResource,
+  {
+    // Provider for APP_INITIALIZER
+    provide: APP_INITIALIZER,
+    useFactory: startupServiceFactory,
+    deps: [NDataSourceService],
+    multi: true
+  },
+  NAuthGuardService,
+  //CORE_REFERENCE_PUSH_TO_PRO_ARRAY
+
+];
+
+/**
+* Routes available for bApp
+*/
+
+// CORE_REFERENCE_PUSH_TO_ROUTE_ARRAY_START
+export const appRoutes = [{path: 'home', component: sessionpageComponent},{path: '', redirectTo: 'home', pathMatch: 'full'},{path: '**', component: PageNotFoundComponent}]
+// CORE_REFERENCE_PUSH_TO_ROUTE_ARRAY_END
---
+++ app/src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    "name": "dev",
+    "properties": {
+        "production": false,
+        "ssdURL": "http://localhost:8081/api/",
+        "tenantName": "neutrinos-training",
+        "appName": "sessionone",
+        "namespace": "com.neutrinos-training.sessionone",
+        "googleMapKey": "AIzaSyCSTnVwijjv0CFRA4MEeS-H6PAQc87LEoU",
+        "useDefaultExceptionUI": true,
+        "isIDSEnabled": "false",
+        "webAppMountpoint": "web",
+        "NGFORAGE_MOBILE_DRIVER": "INDEXED_DB"
+    }
+}
